007f5c7c2a76f33690e266b09389f579c5a7a1df
ThatRapho <147624039+ThatRapho@users.noreply.github.com>
2025-06-20 10:40:56 +0000

    Added props and ternary operators

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,19 +1,49 @@
+import placeholder from '../../img/placeholder.jpg';
+import img1 from '../../img/1.jpg';
+import img2 from '../../img/2.jpg';
+import img3 from '../../img/3.jpg';
+import img4 from '../../img/4.jpg';
+
+
 const Card = () => {
-  const cards = [1, 2, 3, 4];
+  const cardInfo = [
+    {
+      title: 'Mountains',
+      description: 'Explore the beauty of the mountains.',
+      image: img1,
+      URL:  "link1",
+    },
+    {
+      title: 'Ocean',
+      description: 'Dive into the deep blue sea.',
+      image: img2,
+      URL:  "link2",
+    },
+    {
+      title: 'Forest',
+      description: 'Wander through the lush forest.',
+      image: img3,
+      URL:  "link3",
+    },
+    {
+      title: 'Desert',
+      description: 'Feel the heat of the desert.',
+      image: img4,
+      URL:  "link4",
+    },
+  ];
 
   return (
     <div className="container text-center">
       <div className="row">
-        {cards.map((card, index) => (
+        {cardInfo.map((card, index) => (
           <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card h-100">
-              <img src="src/img/1.jpg" className="card-img-top" alt="..." />
+              <img src={card.image ? card.image: placeholder} className="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's content.
-                </p>
-                <a href="#" className="btn btn-primary">Find Out More!</a>
+                <h5 className="card-title">{card.title ? card.title:"Card title missing 😔"}</h5>
+                <p className="card-text">{card.description ? card.description:"Card description missing 😔"}</p>
+                <a href={card.URL} className="btn btn-primary">Find Out More!</a>
               </div>
             </div>
           </div>
